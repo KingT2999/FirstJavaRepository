1dde6f574c907f963430f84358f9ff028a9e359c
KingT2999 <tbektashyan@inbox.ru>
2023-01-18 14:25:32 +0300

    Comments in Main.java file.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,10 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
+        // Random values generator
         Random rand = new Random();
+
+        // Names List
         String[] names = new String[] {
                 "Howdy",
                 "Murphy",
@@ -17,12 +20,14 @@ public class Main {
                 "KingT"
         };
 
+        // Person List Generating
         Person p[] = new Person[5];
 
         for (byte i = 0; i < p.length; i++) {
             p[i] = new Person(names[rand.nextInt(names.length)], rand.nextInt(128), rand.nextInt(200));
         }
 
+        // Person List Printng
         System.out.println("\n------------------------------------------------------\n");
         for (Person pr : p) {
             pr.say_hello();
